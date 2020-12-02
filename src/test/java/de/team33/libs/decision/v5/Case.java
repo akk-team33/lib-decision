531f7306f291562e65a9f0742e5994437a63d85f
KlugeKaindl <akk-gthb@team33.de>
2020-12-02 12:45:29 +0100

    added javadoc

--- src/test/java/de/team33/libs/decision/v5/Case.java
+++ src/test/java/de/team33/libs/decision/v5/Case.java
@@ -2,10 +2,26 @@ package de.team33.libs.decision.v5;
 
 import java.util.Optional;
 
+/**
+ * Represents a case within a case distinction.
+ * <p>
+ * An implementation is best (but not necessarily) as {@code enum}, so that all relevant cases of the case distinction
+ * are covered.
+ *
+ * @param <R> The potential result type.
+ */
 public interface Case<R> {
 
+    /**
+     * In the context of all possible cases of a case distinction, a case has a unique name that identifies it.
+     * <p>
+     * If the cases are implemented as {@code enum}, this method is implemented "automatically".
+     */
     String name();
 
+    /**
+     * A case can be associated with a final result. If not, further cases have to be distinguished.
+     */
     default Optional<R> result() {
         return Optional.empty();
     }
--- src/test/java/de/team33/libs/decision/v5/Cases.java
+++ src/test/java/de/team33/libs/decision/v5/Cases.java
@@ -1,20 +1,40 @@
 package de.team33.libs.decision.v5;
 
-public class Cases {
+/**
+ * A tool to deal with {@link Case}s.
+ */
+public final class Cases {
 
     @SuppressWarnings("rawtypes")
     private static final Case PENDING = () -> "PENDING";
 
+    private Cases() {
+    }
+
+    /**
+     * Returns the {@link Case} that no distinction has yet been made.
+     * This is always at the beginning of a case distinction.
+     */
     public static <R> Case<R> pending() {
         //noinspection unchecked
         return PENDING;
     }
 
+    /**
+     * Returns the opposite of a {@link Case}.
+     * <p>
+     * A {@link Case} leads to a decision, a decision leads to a {@link Case} or its opposite.
+     * <p>
+     * The opposite of a {@link Case} is not a negation in the strictly Boolean sense. Indeed, a {@link Case}
+     * serves as precondition and leads to a decision, which in turn leads to either a {@link Case} or its opposite.
+     * A {@link Case} and its opposite therefore have the same precondition, but behave in the opposite way with regard
+     * to the immediately preceding decision.
+     */
     public static <R> Case<R> not(final Case<R> original) {
         return Opposite.of(original);
     }
 
-    public static <R> Case<R> definite(final Case<R> original, final R result) {
+    static <R> Case<R> definite(final Case<R> original, final R result) {
         return Definite.of(original, result);
     }
 }
